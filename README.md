bc6e9d18fcace21f1feae9fe23ff966cf70a743d
VladanT3 <vladt.t33@gmail.com>
2025-06-20 15:53:25 +0200

    Fixed some mistakes, added more stuff to the script and README

--- README.md
+++ README.md
@@ -6,9 +6,25 @@ At the moment only for Fedora with Gnome, more will be added when I need it.
 
 ## Prerequisites
 - Install Git
-- Make a SSH key and add it to Github (optional)
+- Make a SSH key and add it to Github
 
 ## How to use
 - Clone the repo
 - chmod the script file you need
 - Run it
+- Set zsh as the default shell
+
+### Editing
+To configure Gnome use:
+- For extensions:
+```sh
+dconf dump /org/gnome/shell/extensions/ > gnome_config.conf
+```
+- For keybindings:
+```sh
+dconf dump /org/gnome/desktop/wm/keybindings/ > gnome-keybindings.conf\
+dconf dump /org/gnome/settings-daemon/plugins/media-keys/ >> gnome-keybindings.conf\
+dconf dump /org/gnome/shell/keybindings/ >> gnome-keybindings.conf\
+dconf dump /org/gnome/mutter/keybindings/ >> gnome-keybindings.conf
+```
+```
--- fedora_gnome.sh
+++ fedora_gnome.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Error handling
+set -eo pipefail
+
 # Make sure the starting point is the home dir
 cd ~
 
 # Install JetBrainsMono Nerd Font
 echo "Installing JetBrainsMono..."
-curl -O https://github.coprom/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip
+curl -O https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip
 mkdir -p .fonts
 unzip JetBrainsMono.zip -d ~/.fonts/
 rm JetBrainsMono.zip
@@ -21,14 +24,14 @@ echo "Installing the Browser..."
 curl -fsS https://dl.brave.com/install.sh | sh
 
 # RPM Packages
-dnf copr enable pgdev/ghostty
+sudo dnf copr enable -y pgdev/ghostty
 rpm_packages=(
     go
     python3
     pip
     indent
     neovim
-    fd
+    fd-find
     fzf
     ripgrep
     ghostty
@@ -47,6 +50,10 @@ echo "Installing packages..."
 for package in "${rpm_packages[@]}"; do
     sudo dnf install -y "${package}"
 done
+
+# Add flathub if needed
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
 for package in "${flatpak_packages[@]}"; do
     flatpak install -y flathub "${package}"
 done
@@ -60,6 +67,9 @@ cd nvim
 git switch lazynvim
 cd ~
 
+# Install Oh-My-Posh
+curl -s https://ohmyposh.dev/install.sh | bash -s
+
 # Clone dotfiles repo
 echo "Setting up dotfiles..."
 git clone git@github.com:VladanT3/dotfiles.git
@@ -70,9 +80,12 @@ stow .
 cd ~
 
 # Gnome customization
-echo "Setting up Gnome for cusotmizing"
+echo "Setting up Gnome..."
 cd Get_Started_Anywhere
-dconf load /org/gnome/shell/extensions/ <"gnome_config.txt"
+dconf load /org/gnome/shell/extensions/ <gnome_config.conf
+dconf load / <gnome_keybindings.conf
 cd ~
 mkdir -p .config/gtk-3.0
 mkdir -p .config/gtk-4.0
+
+echo "Done, have fun!"
